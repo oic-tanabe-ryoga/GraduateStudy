b0224c7793c45a24a98c045eb9aedd79882b4c50
oic-tanabe-ryoga <ryo313blackflan@gmail.com>
2016-09-28 11:18:48 +0900

    インプットマネージャ追加

---
+++ Assets/Scripts/InputMgr.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//　ゲームプログラマー３年制コース　田邉崚雅
+//　インプット管理クラス
+public class InputMgr : MonoBehaviour {
+	public static bool mouseLeftButton;
+	public static bool mouseRightButton;
+	public static bool mouseWheelButton;
+	public static bool mouseLeftButtonDown;
+	public static bool mouseRightButtonDown;
+	public static bool mouseWheelButtonDown;
+	public static bool mouseLeftButtonUp;
+	public static bool mouseRightButtonUp;
+	public static bool mouseWheelButtonUp;
+	public static bool wKey;
+	public static bool aKey;
+	public static bool sKey;
+	public static bool dKey;
+	public static bool fire1;
+	public static bool fire2;
+	public static bool fire3;
+	public static bool fire4;
+	public static Vector3 mousePosition;
+	public static float mouseWheelScroll;
+	public static float mouseX;
+	public static float mouseY;
+	public static float mouseXraw;
+	public static float mouseYraw;
+	public static float horizontal;
+	public static float vertical;
+	public static float horizontalRaw;
+	public static float verticalRaw;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		InputBottonTypeA ();
+		InputBottonTypeB ();
+	}
+
+	/// <summary>
+	/// キーボードやマウスその他キーパッドのボタンの入力用
+	/// WASDキーやマウスのクリックその他キーパッドの右側のボタン入力に対応しています。
+	/// </summary>
+	void InputBottonTypeA(){
+
+		//[1]ボタンが押されているかどうかを取得する
+		mouseLeftButton = Input.GetMouseButton(0); 
+		mouseRightButton = Input.GetMouseButton(1);
+		mouseWheelButton = Input.GetMouseButton(2);
+	
+		//[2]ボタンが"1回"押されたかどうかを取得する
+		mouseLeftButtonDown = Input.GetMouseButtonDown(0);
+		mouseRightButtonDown = Input.GetMouseButtonDown(1);
+		mouseWheelButtonDown = Input.GetMouseButtonDown(2);
+	
+		//[3]ボタンが"1回"離されたかどうかを取得する
+		mouseLeftButtonUp = Input.GetMouseButtonUp(0);
+		mouseRightButtonUp = Input.GetMouseButtonUp(1);
+		mouseWheelButtonUp = Input.GetMouseButtonUp(2);
+	
+		//[4]キーボードの場合
+		wKey = Input.GetKey(KeyCode.W);
+		aKey = Input.GetKey(KeyCode.A);
+		sKey = Input.GetKey(KeyCode.S);
+		dKey = Input.GetKey(KeyCode.D);
+	
+		//[5]プロジェクトに当てられた入力設定での取得する
+		//マウスとキーボードの両方を同じボタンに当てることができる
+		//キーボードでは、"Fire1=(Left)Ctrl", "Fire2=(Left)Alt", "(Left)Cmd"となっている
+		fire1 = Input.GetButton("Fire1");
+		fire2 = Input.GetButton("Fire2");
+		fire3 = Input.GetButton("Fire3");
+		fire4 = Input.GetButton("Fire4");
+
+	}
+
+	/// <summary>
+	/// キーボードやマウスその他キーパッドの移動用
+	/// キーボードの矢印キーやマウス移動その他キーパッドのジョイスティックに対応しています。
+	/// </summary>
+
+	void InputBottonTypeB (){
+		//[6]マウスホイールの回転を取得する
+		mouseWheelScroll = Input.GetAxis("Mouse ScrollWheel");
+
+		//[7]マウスの座標を取得する(左下が原点)
+		mousePosition = Input.mousePosition;
+
+		//[8]入力の変位を取得する
+		mouseX = Input.GetAxis("Mouse X");
+		mouseY = Input.GetAxis("Mouse Y");
+
+		//[9]Rawの試験(mouse)では効果が見られず
+		mouseXraw = Input.GetAxisRaw("Mouse X");
+		mouseYraw = Input.GetAxisRaw("Mouse Y");
+
+		//[10]入力の変位を取得する(Keyboard ver.)
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+
+		//[11]Rawの試験(Smoothingされないので-1か1か0になる)
+		horizontalRaw = Input.GetAxisRaw("Horizontal");
+		verticalRaw = Input.GetAxisRaw("Vertical");
+	}
+}
---
+++ Assets/Scripts/TestInputMgr.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestInputMgr : MonoBehaviour {
+	private enum InputType{
+		TypeA,
+		TypeB,
+	}
+	[SerializeField]
+	private InputType InputType_p=InputType.TypeA;
+
+	private string lb = "\n";
+		
+	void Update () {
+		if (InputType_p == InputType.TypeA) {
+			InputTypeAText ();
+		} else if (InputType_p == InputType.TypeB) {
+			InputTypeBText ();
+		}
+	}
+
+	void InputTypeAText(){
+		this.GetComponent<GUIText>().text = "<<  GetMouseButton  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "Left : " + InputMgr.mouseLeftButton + lb;
+		this.GetComponent<GUIText>().text += "Right : " + InputMgr.mouseRightButton + lb;
+		this.GetComponent<GUIText>().text += "Wheel : " + InputMgr.mouseWheelButton + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetMouseButtonDown  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "Left : " + InputMgr.mouseLeftButtonDown + lb;
+		this.GetComponent<GUIText>().text += "Right : " + InputMgr.mouseRightButtonDown + lb;
+		this.GetComponent<GUIText>().text += "Wheel : " + InputMgr.mouseWheelButtonDown + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetMouseButtonUp  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "Left : " + InputMgr.mouseLeftButtonUp + lb;
+		this.GetComponent<GUIText>().text += "Right : " + InputMgr.mouseRightButtonUp + lb;
+		this.GetComponent<GUIText>().text += "Wheel : " + InputMgr.mouseWheelButtonUp + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetKey,Down,Up  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "'W' : " + InputMgr.wKey + lb;
+		this.GetComponent<GUIText>().text += "'A' : " + InputMgr.aKey + lb;
+		this.GetComponent<GUIText>().text += "'S' : " + InputMgr.sKey + lb;
+		this.GetComponent<GUIText>().text += "'D' : " + InputMgr.dKey + lb +lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetButton  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "Fire1 = MouseLeft or LeftCtrl : " + InputMgr.fire1 + lb;
+		this.GetComponent<GUIText>().text += "Fire2 = MouseLeft or LeftAlt : " + InputMgr.fire2 + lb;
+		this.GetComponent<GUIText>().text += "Fire3 = MouseLeft or LeftCmd :  : " + InputMgr.fire3 + lb;
+		this.GetComponent<GUIText>().text += "Fire3 = MouseLeft or LeftCmd :  : " + InputMgr.fire4 + lb;
+
+	}
+	void InputTypeBText(){
+		this.GetComponent<GUIText>().text = "<<  GetAxis(MouseWheel)  >>" + lb;
+		this.GetComponent<GUIText>().text += "Scroll : " + InputMgr.mouseWheelScroll + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  MousePosition(MouseWheel)  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "MousePos : " + InputMgr.mousePosition + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetAxis(MouseX,Y)  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "MouseX : " + InputMgr.mouseX + lb;
+		this.GetComponent<GUIText>().text += "MouseY : " + InputMgr.mouseY + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetAxisRaw(MouseX,Y)  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "MouseX-Raw: " + InputMgr.mouseXraw + lb;
+		this.GetComponent<GUIText>().text += "MouseY-Raw: " + InputMgr.mouseYraw + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetAxis(Horizontal,Vertical)  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "Horizontal : " + InputMgr.horizontal + lb;
+		this.GetComponent<GUIText>().text += "Vertical : " + InputMgr.vertical + lb + lb;
+
+		this.GetComponent<GUIText>().text += "<<  GetAxisRaw(Horizontal,Vertical)  >>" + lb;
+
+		this.GetComponent<GUIText>().text += "Horizontal Raw: " + InputMgr.horizontalRaw + lb;
+		this.GetComponent<GUIText>().text += "Vertical Raw: " + InputMgr.verticalRaw + lb;
+
+	}
+}
